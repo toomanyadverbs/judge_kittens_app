4e3273efb415234791dd07503a7c37585f628c75
toomanyadverbs <lark.grimes@gmail.com>
2015-06-03 16:16:55 -0400

    Fixed up toggled function

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-/*global judgeKittens, $*/
+/* global judgeKittens */
 "use strict";
 
 //define global variable
@@ -14,15 +14,19 @@ $(document).ready(function(){
 
 //getRandomInt(Cat.first[id], Cat.last[id])
 
-  $.ajax({
-    type: 'GET',
-    url: 'http://localhost:5000/Cats/6'
+    $.ajax({
+      type: 'GET',
+      url: 'https://floating-refuge-2919.herokuapp.com/Cats/1'
   }).done(function(response) {
-  var cat = JSON.parse(response);
-  var newImg = $("#catOne").append("img");
-  newImg.src = response.pic
-  });
-
+    // we expect response to be a JSON string which, when parsed,
+    // represents our cat.
+    var cat = JSON.parse(response);
+    $(catNameId).text(cat.name);
+    $(catOwnerId).text(cat.owner);
+    $(catImageId).attr("src", cat.pic);
+      }).fail(function(error){
+        console.log('GET /cat/:id call failed, error is ' + error);
+    });
 
   $(".cuteToggle").click(function(){
     $("div").find(".selectedJudgement").addClass("col-md-3").removeClass("selectedJudgement col-md-5");
@@ -32,7 +36,7 @@ $(document).ready(function(){
   });
 
   $(".majToggle").click(function(){
-    $("div").find(".selectedJudgement").addClass("col-md-3").removeClass("selectedJudgement");
+    $("div").find(".selectedJudgement").addClass("col-md-3").removeClass("selectedJudgement col-md-5");
     $(this).removeClass("col-md-3").addClass("selectedJudgement col-md-5");
     $("div").find(".toggle").removeClass("cute").addClass("maj");
     $("div").find(".toggleQuestion").html("<h5>Which cat is more majestic?</h5>")
