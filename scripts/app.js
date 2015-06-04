fd7a598213c37b5e0fb9599a335ebd8e11c8f2bc
toomanyadverbs <lark.grimes@gmail.com>
2015-06-04 19:16:57 -0400

    Styled, added login section

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,7 @@
 //define global variable
 var judgeKittens = judgeKittens || {};
 
+//Random Integer func to get random cat pics:
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
@@ -12,27 +13,16 @@ function getRandomInt(min, max) {
 //call functions after document is ready
 $(document).ready(function(){
 
-//getRandomInt(Cat.first[id], Cat.last[id])
-
-    $.ajax({
-      type: 'GET',
-      url: 'https://floating-refuge-2919.herokuapp.com/Cats/1'
-  }).done(function(response) {
-    // we expect response to be a JSON string which, when parsed,
-    // represents our cat.
-    var cat = JSON.parse(response);
-    $(catNameId).text(cat.name);
-    $(catOwnerId).text(cat.owner);
-    $(catImageId).attr("src", cat.pic);
-      }).fail(function(error){
-        console.log('GET /cat/:id call failed, error is ' + error);
-    });
-
-  $(".cuteToggle").click(function(){
-    $("div").find(".selectedJudgement").addClass("col-md-3").removeClass("selectedJudgement col-md-5");
-    $(this).removeClass("col-md-3").addClass("selectedJudgement col-md-5");
-    $("div").find(".toggle").removeClass("maj").addClass("cute");
-    $("div").find(".toggleQuestion").html("<h5>Which cat is cuter?</h5>")
+  $.ajax({
+    url : 'http://localhost:3000/cats/1',
+    type: 'GET',
+    data: 'json',
+  }).done(function(cat){
+    console.log(cat);
+    $('#catName').html("<p>Cat Name:" + cat.name + "</p>");
+    $('#catImageId').attr("src", "http://localhost:3000/" + cat.pic);
+  }).fail(function(){
+    console.log('error is' + error)
   });
 
   $(".majToggle").click(function(){
@@ -42,5 +32,40 @@ $(document).ready(function(){
     $("div").find(".toggleQuestion").html("<h5>Which cat is more majestic?</h5>")
   });
 
+  $(".cuteToggle").click(function(){
+    $("div").find(".selectedJudgement").addClass("col-md-3").removeClass("selectedJudgement col-md-5");
+    $(this).removeClass("col-md-3").addClass("selectedJudgement col-md-5");
+    $("div").find(".toggle").removeClass("maj").addClass("cute");
+    $("div").find(".toggleQuestion").html("<h5>Which cat is cuter?</h5>");
+  });
+
+
+
 
 }); //<-document ready
+
+//jquery authenticate and get
+$(function(){
+  $('#get-token').on('click', function(){
+    $.ajax('http://localhost:3000/login',{
+      contentType: 'application/json',
+      processData: false,
+      data: JSON.stringify({
+        credentials: {
+          email: $('#email').val(),
+          password: $('#password').val()
+        }
+      }),
+      dataType: "json",
+      method: "POST"
+    }).done(function(data, textStatus) {
+      $('#token').val(textStatus == 'nocontent' ? 'login failed' : data['token']);
+      localStorage.setItem('token', data.token);
+      console.log(data);
+    }).fail(function(jqxhr, textStatus, errorThrown){
+      console.log(textStatus);
+      console.log(errorThrown);
+    });
+  });
+
+});
