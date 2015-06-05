808ffd38f4e1ebed96820fb2fa25c9d28084ec39
toomanyadverbs <lark.grimes@gmail.com>
2015-06-05 16:57:28 -0400

    Added first patch ajax call; fixed a few styles

--- scripts/app.js
+++ scripts/app.js
@@ -11,7 +11,6 @@ function getRandomInt(min, max) {
 
 function getTwoRandomInt(min, max) {
   var first = Math.floor(Math.random() * (max - min)) + min;
-
   do {var second = Math.floor(Math.random() * (max - min)) + min }
   while (first === second);
   return {
@@ -20,15 +19,16 @@ function getTwoRandomInt(min, max) {
   }
 }
 
-var randomIntegers = getTwoRandomInt(0, 9);
+var randomIntegers = getTwoRandomInt(1, 15);
 var first = randomIntegers.first;
 var second = randomIntegers.second;
 
 //call functions after document is ready
 $(document).ready(function(){
 
-var catUrlOne = 'http://localhost:3000/cats/' + first
-var catUrlTwo = 'http://localhost:3000/cats/' + second
+var catUrl = 'http://localhost:3000/cats/';
+var catUrlOne = catUrl + first;
+var catUrlTwo = catUrl + second
 
   $.ajax({
     url: catUrlOne,
@@ -36,7 +36,8 @@ var catUrlTwo = 'http://localhost:3000/cats/' + second
     data: 'json',
   }).done(function(cat_data_one){
     console.log(cat_data_one);
-    $('#catLabelOne').html('Name: ' + cat_data_one.name + '<br>Owner: ' + cat_data_one.owner);
+    $('#catLabelOne').html('Name: ' + cat_data_one.name + ', Owner: ' + cat_data_one.owner);
+    $('#catImgIdOne').data('id', cat_data_one.id).data('ct_jdgmts_cute', cat_data_one.ct_jdgmts_cute);
     $('#catImgIdOne').attr("src", "http://localhost:3000/" + cat_data_one.pic);
   }).fail(function(){
     console.log('error is' + error)
@@ -48,7 +49,8 @@ var catUrlTwo = 'http://localhost:3000/cats/' + second
     data: 'json',
   }).done(function(cat_data_two){
     console.log(cat_data_two);
-    $('#catLabelTwo').html('Name: ' + cat_data_two.name + '<br>Owner: ' + cat_data_two.owner);
+    $('#catLabelTwo').html('Name: ' + cat_data_two.name + ', Owner: ' + cat_data_two.owner);
+    $('#catImgIdTwo').data('id', cat_data_two.id).data('ct_jdgmts_cute', cat_data_two.ct_jdgmts_cute);
     $('#catImgIdTwo').attr("src", "http://localhost:3000/" + cat_data_two.pic);
   }).fail(function(){
     console.log('error is' + error)
@@ -69,8 +71,29 @@ var catUrlTwo = 'http://localhost:3000/cats/' + second
     $("div").find(".toggleQuestion").html("<h5>Which cat is cuter?</h5>");
   });
 
+  var addWin = {cat: {
+    }};
 
+  $("#catImgIdOne").click(function(){
+    var catID = $('#catImgIdOne').data('id');
+    var catJdgmtCount = $('#catImgIdOne').data('ct_jdgmts_cute');
+    addWin.cat.id = catID;
+    addWin.cat.ct_jdgmts_cute = catJdgmtCount + 1;
 
+    debugger;
+    $.ajax({
+      url: catUrlOne,
+      type: 'PATCH',
+      dataType: 'json',
+      data: addWin
+    })
+    .done(function(){
+      console.log('Added Judgement')
+      $('.catOne').children(".ribbons").html('Winner! Judgements: ' + addWin.cat.ct_jdgmts_cute);
+    })
+    .fail(function(){
+      console.log('Error in Posting');
+    });  });
 
 }); //<-document ready
 
